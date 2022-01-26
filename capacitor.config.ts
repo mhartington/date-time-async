7325f95c1d68efd2d9bd50a852d4634bd49d1f41
Mike Hartington <mikehartington@gmail.com>
2022-01-26 12:03:27 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tmp-ng',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
